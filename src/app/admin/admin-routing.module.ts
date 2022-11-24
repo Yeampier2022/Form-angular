3eaf2fcfdf2c96359ce9376bc8d876630ac04b09
Yeampier2022 <yeampierjesusa@gmail.com>
2022-11-24 13:50:50 -0400

    Modulo-Finalizado

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,8 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { BasicFormComponent } from './components/basic-form/basic-form.component';
 
-
-
 const routes: Routes = [
   {
     path: '',
@@ -26,7 +24,7 @@ const routes: Routes = [
       {
         path: 'basic',
         component: BasicFormComponent
-      },
+      }
     ]
   }
 ];
